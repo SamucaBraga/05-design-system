b111ffb64a6b8a6c1a77081f0a25723f8693187a
Samuel Braga <samuelbgf@hotmail.com>
2023-07-06 08:25:52 -0300

    feat: Add core components

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data displat/Avatar',
+  component: Avatar,
+  tags: ['autodocs'],
+  args: {
+    src: 'https://github.com/SamucaBraga.png',
+    alt: 'Samuel Braga',
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
